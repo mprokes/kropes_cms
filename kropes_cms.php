4ffaa6504a5564fe067a882dd77220a556c2b0f4
Michal Prokes <michal@kropes.cz>
2012-10-22 21:09:42 +0200

    Clear mess in code

--- kropes_cms.php
+++ kropes_cms.php
@@ -53,59 +53,6 @@ class Kropes_cms{
 	}
 
 
-
-//TODO
-	function admin_page_action(){
-if ( empty($_POST) )
-	                        return;
-if ( isset($_POST['aax']) ) {
-	                        // @TODO: Uploaded files are not removed here.
-	                        check_admin_referer('custom-background-remove', '_wpnonce-custom-background-remove');
-	                        set_theme_mod('aax', $_POST['aax']);
-	                        $this->updated = true;
-	                        return;
-	                }
-	}
-
-
-// TODO
-	function admin_page(){
-?>
-<?php if ( !empty($this->updated) ) { ?>
-	<div id="message" class="updated">
-	<p><?php printf( __( 'Background updated. <a href="%s">Visit your site</a> to see how it looks.' ), home_url( '/' ) ); ?></p>
-	</div>
-	<?php } ?>
-	<form method="post" action="">
-	<input name="aax" value="<? echo get_theme_mod('aax', '');?>">
-	<?php wp_nonce_field('custom-background-remove', '_wpnonce-custom-background-remove'); ?>
-	<?php submit_button( __( 'Remove Background Image' ), 'button', 'remove-background', false ); ?><br/>
-	<?php _e('This will remove the background image. You will not be able to restore any customizations.') ?>
-	</form>
-<?php
-	}
-
-//TODO
-	function admin_page_load(){
-get_current_screen()->add_help_tab( array(
-	                        'id'      => 'overview',
-	                        'title'   => __('Overview'),
-	                        'content' =>
-	                                '<p>' . __( 'You can customize the look of your site without touching any of your theme&#8217;s code by using a custom background. Your background can be an image or a color.' ) . '</p>' .
-	                                '<p>' . __( 'To use a background image, simply upload it, then choose your display options below. You can display a single instance of your image, or tile it to fill the screen. You can have your background fixed in place, so your site content moves on top of it, or you can have it scroll with your site.' ) . '</p>' .
-	                                '<p>' . __( 'You can also choose a background color. If you know the hexadecimal code for the color you want, enter it in the Background Color field. If not, click on the Select a Color link, and a color picker will allow you to choose the exact shade you want.' ) . '</p>' .
-	                                '<p>' . __( 'Don&#8217;t forget to click on the Save Changes button when you are finished.' ) . '</p>'
-	                ) );
-
-get_current_screen()->set_help_sidebar(
-	                        '<p><strong>' . __( 'For more information:' ) . '</strong></p>' .
-	                       '<p>' . __( '<a href="http://codex.wordpress.org/Appearance_Background_Screen" target="_blank">Documentation on Custom Background</a>' ) . '</p>' .
-	                        '<p>' . __( '<a href="http://wordpress.org/support/" target="_blank">Support Forums</a>' ) . '</p>'
-	                );
-	}
-
-
-
 	function extend_author_profile( $fields )
 	{
 		$fields['phone'] = 'Telefon';
